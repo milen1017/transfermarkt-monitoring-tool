763fc9cf041ce25932881d524279718bb5c1a99f
milen1017 <mn.krumov@gmail.com>
2024-05-22 18:53:20 +0300

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-// src/App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home.jsx";
 import AddLeague from "./components/AddLeague.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/add-league" element={<AddLeague />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-league" element={<AddLeague />} />
+      </Routes>
+    </div>
   );
 }
 
