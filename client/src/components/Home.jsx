22cd79b6242c91bf120ebc4813d1d3960a223d7f
milen1017 <mn.krumov@gmail.com>
2024-05-22 14:53:52 +0300

    Add per-league toggle functionality for showing/hiding checked and unchecked transfers

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
   const [leagues, setLeagues] = useState([]);
   const [loading, setLoading] = useState(false);
   const [transfers, setTransfers] = useState({});
-  const [showChecked, setShowChecked] = useState(false);
+  const [showCheckedTransfers, setShowCheckedTransfers] = useState({});
+  const [showUncheckedTransfers, setShowUncheckedTransfers] = useState({});
 
   const fetchLeagues = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/api/leagues');
+      const response = await axios.get("http://localhost:3000/api/leagues");
       setLeagues(response.data);
     } catch (error) {
-      console.error('Error fetching leagues:', error);
+      console.error("Error fetching leagues:", error);
     }
   };
 
@@ -20,13 +21,21 @@ function Home() {
     fetchLeagues();
   }, []);
 
+  useEffect(() => {
+    if (leagues.length > 0) {
+      leagues.forEach((league) => {
+        fetchTransfers(league.id);
+      });
+    }
+  }, [leagues]);
+
   const handleScrapeAndSave = async () => {
     setLoading(true);
     try {
-      await axios.post('http://localhost:3000/api/scrape-transfers');
+      await axios.post("http://localhost:3000/api/scrape-transfers");
       fetchLeagues(); // Re-fetch leagues to update the data
     } catch (error) {
-      console.error('Error during scraping and saving transfers:', error);
+      console.error("Error during scraping and saving transfers:", error);
     } finally {
       setLoading(false);
     }
@@ -34,8 +43,13 @@ function Home() {
 
   const fetchTransfers = async (leagueID) => {
     try {
-      const response = await axios.get(`http://localhost:3000/api/transfers/${leagueID}`);
-      setTransfers((prevTransfers) => ({ ...prevTransfers, [leagueID]: response.data }));
+      const response = await axios.get(
+        `http://localhost:3000/api/transfers/${leagueID}`
+      );
+      setTransfers((prevTransfers) => ({
+        ...prevTransfers,
+        [leagueID]: response.data,
+      }));
     } catch (error) {
       console.error(`Error fetching transfers for league ${leagueID}:`, error);
     }
@@ -43,7 +57,10 @@ function Home() {
 
   const handleCheckTransfer = async (transferID, checked) => {
     try {
-      const response = await axios.put(`http://localhost:3000/api/transfers/${transferID}`, { checked });
+      const response = await axios.put(
+        `http://localhost:3000/api/transfers/${transferID}`,
+        { checked }
+      );
       const updatedTransfer = response.data;
 
       setTransfers((prevTransfers) => {
@@ -54,12 +71,22 @@ function Home() {
         return { ...prevTransfers, [leagueID]: updatedTransfers };
       });
     } catch (error) {
-      console.error('Error updating transfer:', error);
+      console.error("Error updating transfer:", error);
     }
   };
 
-  const toggleShowChecked = () => {
-    setShowChecked(!showChecked);
+  const toggleShowCheckedTransfers = (leagueID) => {
+    setShowCheckedTransfers((prevState) => ({
+      ...prevState,
+      [leagueID]: !prevState[leagueID],
+    }));
+  };
+
+  const toggleShowUncheckedTransfers = (leagueID) => {
+    setShowUncheckedTransfers((prevState) => ({
+      ...prevState,
+      [leagueID]: !prevState[leagueID],
+    }));
   };
 
   return (
@@ -67,48 +94,98 @@ function Home() {
       <h1>Home Page</h1>
       <p>Welcome to the Home Page.</p>
       <button onClick={handleScrapeAndSave} disabled={loading}>
-        {loading ? 'Processing...' : 'Scrape and Save Transfers'}
+        {loading ? "Processing..." : "Scrape and Save Transfers"}
       </button>
       <h2>Leagues</h2>
       {leagues.length > 0 ? (
         <ul>
           {leagues.map((league) => (
             <li key={league._id}>
-              <strong>{league.leagueName}</strong> id: {league.id} : <a href={league.url}>link</a>
-              <button onClick={() => fetchTransfers(league.id)}>Show Transfers</button>
-              <h3>Transfers:</h3>
-              <button onClick={toggleShowChecked}>
-                {showChecked ? 'Hide Checked Transfers' : 'Show Checked Transfers'}
+              <a href={league.url} target="_blank" rel="noopener noreferrer">
+                {league.leagueName}
+              </a>{" "}
+              id: {league.id}
+              <button onClick={() => toggleShowUncheckedTransfers(league.id)}>
+                {showUncheckedTransfers[league.id]
+                  ? "Hide Unchecked Transfers"
+                  : "Show Unchecked Transfers"}
               </button>
-              {transfers[league.id] ? (
-                transfers[league.id].length > 0 ? (
-                  <ul>
-                    {transfers[league.id]
-                      .filter((transfer) => !transfer.checked || showChecked)
-                      .map((transfer) => (
-                        <li key={transfer._id}>
-                          <strong>{transfer.name}</strong> - {transfer.position}
-                          <br />
-                          From: {transfer.fromTeam} To: {transfer.toTeam}
-                          <br />
-                          Date: {new Date(transfer.date).toLocaleDateString()}
-                          <br />
-                          <label>
-                            <input
-                              type="checkbox"
-                              checked={transfer.checked}
-                              onChange={(e) => handleCheckTransfer(transfer._id, e.target.checked)}
-                            />
-                            Checked
-                          </label>
-                        </li>
-                      ))}
-                  </ul>
-                ) : (
-                  <p>No transfers found for this league.</p>
-                )
-              ) : (
-                <p>Click "Show Transfers" to load transfers.</p>
+              <button onClick={() => toggleShowCheckedTransfers(league.id)}>
+                {showCheckedTransfers[league.id]
+                  ? "Hide Checked Transfers"
+                  : "Show Checked Transfers"}
+              </button>
+              {transfers[league.id] && (
+                <div>
+                  {showUncheckedTransfers[league.id] && (
+                    <div>
+                      <h3>Unchecked Transfers</h3>
+                      <ul>
+                        {transfers[league.id]
+                          .filter((transfer) => !transfer.checked)
+                          .map((transfer) => (
+                            <li key={transfer._id}>
+                              <strong>{transfer.name}</strong> -{" "}
+                              {transfer.position}
+                              <br />
+                              From: {transfer.fromTeam} To: {transfer.toTeam}
+                              <br />
+                              Date:{" "}
+                              {new Date(transfer.date).toLocaleDateString()}
+                              <br />
+                              <label>
+                                <input
+                                  type="checkbox"
+                                  checked={transfer.checked}
+                                  onChange={(e) =>
+                                    handleCheckTransfer(
+                                      transfer._id,
+                                      e.target.checked
+                                    )
+                                  }
+                                />
+                                Checked
+                              </label>
+                            </li>
+                          ))}
+                      </ul>
+                    </div>
+                  )}
+                  {showCheckedTransfers[league.id] && (
+                    <div>
+                      <h3>Checked Transfers</h3>
+                      <ul>
+                        {transfers[league.id]
+                          .filter((transfer) => transfer.checked)
+                          .map((transfer) => (
+                            <li key={transfer._id}>
+                              <strong>{transfer.name}</strong> -{" "}
+                              {transfer.position}
+                              <br />
+                              From: {transfer.fromTeam} To: {transfer.toTeam}
+                              <br />
+                              Date:{" "}
+                              {new Date(transfer.date).toLocaleDateString()}
+                              <br />
+                              <label>
+                                <input
+                                  type="checkbox"
+                                  checked={transfer.checked}
+                                  onChange={(e) =>
+                                    handleCheckTransfer(
+                                      transfer._id,
+                                      e.target.checked
+                                    )
+                                  }
+                                />
+                                Checked
+                              </label>
+                            </li>
+                          ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
               )}
             </li>
           ))}
