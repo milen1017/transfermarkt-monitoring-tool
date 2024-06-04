a2c1d67d247aec12d5281d596c5efa49a141182a
milen1017 <mn.krumov@gmail.com>
2024-06-04 19:36:36 +0300

    add cors origin

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDB = require("./database");
 const urlRoutes = require("./routes/leaguesRouter.js");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -11,16 +11,22 @@ const app = express();
 connectDB();
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:5173'||"https://transfermarkt-monitoring-tool-m6g0zflzt-milen1017s-projects.vercel.app" || 'http://localhost:3000' , // Replace with your front-end URL
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://transfermarkt-monitoring-tool-m6g0zflzt-milen1017s-projects.vercel.app",
+      "https://transfermarkt-monitoring-tool-5p5ki1mwm-milen1017s-projects.vercel.app/", // Add any other allowed origins
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 // Use routes
 app.use(urlRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
